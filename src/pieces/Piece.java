b09fff3eb695e99477e33d7471766b2c09330ca8
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-22 10:16:21 +0100

    Ajout Classe Piece

---
+++ src/pieces/Piece.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+package pieces;
+
+import util.Color;
+import util.Position;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ */
+public class Piece {
+
+		private Position position;
+		private char symbol;
+		private Color color;
+		private String Name;
+		private Chessboard board;
+
+
+}
