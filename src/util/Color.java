1b62a42536c37fedcf3fb0d2385c37bd70cd7ace
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-22 10:19:21 +0100

    Ajout Enumeration Color, délaration de ses attributs et de son constructeur

---
+++ src/util/Color.java
@@ -0,0 +1,16 @@
+/**
+ *
+ */
+package util;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ * Représente les couleurs des pièces du jeu d'échecs.
+ */
+public enum Color {
+	BLACK, WHITE;
+	Color(){
+
+	}
+}
