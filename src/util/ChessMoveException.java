160a303d5c499a52f19e46b861824bb9f4cd780f
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-22 10:20:30 +0100

    Ajout classe ChessMoveException et déclaration partielle de son constructeur

---
+++ src/util/ChessMoveException.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package util;
+
+/**
+ * @author ALIKOU DONGMO NELSON
+ *
+ * Lancée lorsqu'un déplacement est invalide.
+ */
+public class ChessMoveException extends Exception{
+
+	public ChessMoveException(String message, Position position1, Position Position2) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
